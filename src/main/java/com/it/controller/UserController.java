ea6481faa4108fcbba59fd8f0f9f21ac3ab20dc8
Artit <po_mal@hotmail.co.th>
2021-04-11 14:07:56 +0700

    - add delete user

--- src/main/java/com/it/controller/UserController.java
+++ src/main/java/com/it/controller/UserController.java
@@ -5,6 +5,7 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -101,6 +102,12 @@ public class UserController {
 		}else {
 			return ResponseEntity.badRequest().body(null);
 		}
+	}
+	
+	@DeleteMapping("/users/{userId}")
+	public ResponseEntity<String> deleteUserByUserId(@PathVariable("userId") String userId) {
+		userRepository.deleteById(userId);
+		return ResponseEntity.ok("SUCCESS");
 	}
 	
 }//end
